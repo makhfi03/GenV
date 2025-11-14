ca5a850881a23c37a9baeaaebe462bf5969fa052
makhfi03 <ghita.makhfi.21@gmail.com>
2025-11-14 09:17:57 +0100

    Add files via upload
    workshop

--- genv-squad/script.js
+++ genv-squad/script.js
@@ -1,39 +1,53 @@
+let formule = document.getElementById("modal");
+formule.style.display= "none";
 let apprenantsData;
-async function apprenant(file){
-    let data = await fetch(file);
-    let apprenantsData = await data.json();
+const ajouter = [];
 
-    let apprenantList = [];
+fetch("/json.json")
+.then((response) => response.json())
+.then((apprenant) =>{
+    afficherList(apprenant);
+})
 
-    apprenantsData.forEach(apprenant => {
-        apprenantList.push(apprenant);
-    });
-    
-    localStorage.setItem("apprenants",JSON.stringify(apprenantList));
-}
-apprenant("json.json");
-
-function afficherList(){
-let list = localStorage.getItem("apprenants");
-let appList = JSON.parse(list);
+function afficherList(apprenant){
+const list = localStorage.getItem("apprenants");
+const appList = [...apprenant,...list];
 appList.forEach(element =>{
-    let affichage = document.getElementById("carte_liste").innerHTML+=`
-    <div class="block">
+    let affichage = document.createElement('div')
+    affichage.className = "block"
+    affichage.innerHTML = `
         <ul>
         <div class="image" >
             <img style="width: 6rem; height: 6rem; border-radius: 50%;" src="${element.photo}" alt="photo" class="photo">
         </div>
-            <h3 id="nom">${element.nom}</h3>
-            <h3 id="prenom">${element.prenom}</h3>
+            <h3 id="nom-prenom">${element.nom} ${element.prenom}</h3>
             <p id="email">${element.email}</p>
-            <small id="age">${element.age}</small>
+            <small id="age">${element.age} ans</small>
         </ul>
         </div>
     `
-
-    console.log(affichage);
+    document.querySelector(".carte_liste_apprenant").appendChild(affichage)
 });
-
-// console.log(appList);
 }
-afficherList();
+
+
+let openbtn = document.getElementById("ajouter");
+let closebtn = document.getElementById("save");
+openbtn.addEventListener("click", () => formule.style.display="flex");
+closebtn.addEventListener("click", () => formule.style.display="none");
+
+
+
+document.getElementById('formulaire').addEventListener("submit", (event) =>{
+    event.preventDefault();
+    const add={
+    photo: document.getElementById("imgInput").value,
+    nom: document.getElementById("nomInput").value,
+    prenom: document.getElementById("prenomInput").value,
+    email: document.getElementById("emailInput").value,
+    age: document.getElementById("ageInput").value
+    }
+    ajouter.push(add);
+    localStorage.setItem("element", JSON.stringify(ajouter));
+    afficherList(ajouter);
+})
